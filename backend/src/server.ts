1016aeedb1274b1085524601922cbcd35627864f
Novpa <aa.novparadiputra@binus.ac.id>
2026-03-25 14:53:11 +0800

    chore/generate prisma model

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 
 const app: Express = express();
 
-const PORT = 8000;
+const PORT = 9000;
 
 // json middleware
 app.use(express.json());
@@ -16,13 +16,14 @@ app.use(cookieParser());
 //cors middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // Sesuaikan dengan URL Frontend kamu
-    credentials: true, // WAJIB TRUE agar cookie bisa lewat
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Izinkan OPTIONS
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
+//app config
 app.listen(PORT, () => {
   console.log(`🦄 Server is running in port ${PORT}`);
 });
