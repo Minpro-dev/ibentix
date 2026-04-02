a13f2f8dd7a088b28e541fe983b032b9f2863840
Novpa <aa.novparadiputra@binus.ac.id>
2026-04-03 01:53:56 +0700

    ref/pulling req.file to prevent undefined value

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -5,7 +5,7 @@ import { uploadSingle } from "../utils/cloudinaryUploader";
 
 export const authController = {
   signup: catchAsync(async (req: Request, res: Response) => {
-    const user = await authService.registerUser(req.body, req.file);
+    const user = await authService.registerUser(req.body, req?.file);
 
     return res.status(200).json({
       status: "success",
