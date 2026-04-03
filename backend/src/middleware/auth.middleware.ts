486ac909085d3a09656b2d61bf4ea098422b8999
Novpa <aa.novparadiputra@binus.ac.id>
2026-04-03 19:18:02 +0700

    feat/implement authentication & authorization middleware

---
+++ backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from "express";
+import { TokenPayload, verifyAccessToken } from "../utils/token.util";
+import { AppError } from "../utils/AppError";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: TokenPayload;
+    }
+  }
+}
+
+// AUTHENTICATION
+export const authentication = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const requestHeader = req.headers.authorization;
+
+  if (!requestHeader || !requestHeader.startsWith("Bearer")) {
+    throw new AppError(401, "Unauthorized action");
+  }
+
+  const token = req.headers.authorization?.split(" ")[1] as string;
+
+  const decoded = verifyAccessToken(token);
+
+  req.user = {
+    userId: decoded.userId,
+    role: decoded.role,
+    fullName: decoded.fullName,
+  };
+
+  next();
+};
+
+export const authorization = (...allowedRoles: string[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!allowedRoles.includes(req.user?.role as string)) {
+      throw new AppError(403, "Access denied");
+    }
+
+    next();
+  };
+};
