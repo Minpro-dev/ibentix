2572b0897cbb4f5da69eca34bdc5c3f0acb8f9fc
Novpa <aa.novparadiputra@binus.ac.id>
2026-04-05 21:02:54 +0700

    chore/setting up axios instance

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,6 +1,60 @@
 import axios from "axios";
+import { useAuthStore } from "../store/useAuthStore";
 
 export const api = axios.create({
   baseURL: "http://localhost:8000/api",
   withCredentials: true,
 });
+
+// REQUEST INTERCEPTORS - add access token in every api request
+api.interceptors.request.use((config) => {
+  const token = useAuthStore.getState().accessToken;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// RESPONSE INTERCEPTORS
+api.interceptors.response.use(
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  (response) => response,
+  async (error) => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    const originalRequest = error.config;
+    const isRefreshRequest = originalRequest.url?.includes("/auth/refresh");
+
+    if (
+      error.response.status === 401 &&
+      !originalRequest._retry &&
+      !isRefreshRequest
+    ) {
+      //error is 401, nevery tried, it's not from /auth/refresh
+      originalRequest._retry = true;
+
+      try {
+        const { data } = await axios.get(
+          "http://localhost:8000/api/auth/refresh",
+          { withCredentials: true },
+        );
+        // console.log("data interceptors", data);
+
+        const accessToken = data.data.accessToken;
+        useAuthStore.getState().setAuth(accessToken, data.data.user);
+
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+        return api(originalRequest);
+      } catch (refreshError) {
+        useAuthStore.getState().clearAuth();
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export default api;
---
+++ frontend/src/store/useAuthStore.ts
@@ -0,0 +1,33 @@
+import { create } from "zustand";
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: "ATTENDEE" | "ORGANIZER";
+  createdAt: string;
+}
+
+interface State {
+  accessToken: string | null;
+  user: User | null;
+  isInitializing: boolean;
+}
+
+interface Action {
+  setAuth: (token: string, user: User) => void;
+  clearAuth: () => void;
+  setInitializing: (boolean: boolean) => void;
+}
+
+// ----- STORE
+export const useAuthStore = create<Action & State>((set) => ({
+  user: null,
+  accessToken: null,
+  isInitializing: true,
+
+  setAuth: (token, user) => set({ accessToken: token, user }),
+  clearAuth: () => set({ accessToken: null, user: null }),
+  setInitializing: (boolean) => set({ isInitializing: boolean }),
+}));
