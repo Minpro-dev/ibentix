263bb07c272fdbd742ccf80322d124469fe93575
rannyfafily-ai <rannyfafily@gmail.com>
2026-04-09 09:41:55 +0700

    adding wishlist router

---
+++ backend/src/routers/wishlist.route.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+  toggleWishlist,
+  getWishlist,
+  checkWishlist,
+} from "../controllers/wishlist.controller";
+import { authentication, authorization } from "../middleware/auth.middleware";
+
+const router = Router();
+
+// LIKE / UNLIKE
+router.post("/:eventId", authentication, toggleWishlist);
+
+// GET MY WISHLIST
+router.get("/", authentication, getWishlist);
+
+// CHECK STATUS
+router.get("/:eventId/status", authentication, checkWishlist);
+
+export default router;
