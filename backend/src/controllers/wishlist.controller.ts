18214cbf19dc4802e098a97fd8b41ad859d8d1e3
rannyfafily-ai <rannyfafily@gmail.com>
2026-04-09 09:39:54 +0700

    adding wishlist controller

---
+++ backend/src/controllers/wishlist.controller.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from "express";
+import {
+  toggleWishlistService,
+  getWishlistService,
+  checkWishlistService,
+} from "../services/wishlist.service";
+
+// 1. TOGGLE LIKE
+export const toggleWishlist = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.user.id;
+    const { eventId } = req.params;
+
+    const data = await toggleWishlistService(userId, eventId);
+
+    res.json({ success: true, ...data });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// 2. GET MY WISHLIST
+export const getWishlist = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.user.id;
+
+    const data = await getWishlistService(userId);
+
+    res.json({ success: true, data });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// 3. CHECK LIKE STATUS
+export const checkWishlist = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.user.id;
+    const { eventId } = req.params;
+
+    const data = await checkWishlistService(userId, eventId);
+
+    res.json({ success: true, ...data });
+  } catch (err) {
+    next(err);
+  }
+};
